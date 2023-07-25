cc72d5a6eb3455f67543a420735150efa0abe958
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-07-25 08:36:36 -0500

    Modify the src file add the api

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import post from '../api/post';
+import axios from 'axios';
 
-const Player = (props) => {
 
+const url = `https://www.balldontlie.io/api/v1/players`
+const Player = () => {
+
+    const [pdata, setPdata] = useState({});
+
+    useEffect(() =>{
+
+        const fetchPost = async () => {
+
+            try {
+                const response = await axios.get(url);
+                setPdata(response.data);
+                console.log(pdata.data[4]);
+
+            }catch(err){
+                console.log(err.response);
+            }
+        }
+    fetchPost();    
     
+  },[])
+
+  
+
   return (
     <section>
         <h1>PLayers section</h1>
+        <h1>{pdata.data[4].team.city}</h1>
+
     </section>
   )
 }
